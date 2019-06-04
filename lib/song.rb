9420e5bfbbee8aad23a32e9891770a34328095d2
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-04 13:40:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
   
   def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
+    artist = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
     song.artist = artist
