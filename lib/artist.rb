e0778b5b7a8b42f291f1f984f895cfeee3e37375
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-04 12:17:20 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist 
+  
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def save
+    @@all << name 
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  
+  attr_accessor :name, :genre 
+  
+  def initialize(name)
+    @name = name
+  end
+end
