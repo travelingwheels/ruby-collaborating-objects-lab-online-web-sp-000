1b1ee694ba8fd731258223edc82cd3cbf056f914
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-04 13:40:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
-    song.artist_name = artist_name
+    song.artist = artist
     song
   end
 end
